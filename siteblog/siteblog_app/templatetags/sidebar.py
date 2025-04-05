b886101c8751803a13acc5601548b8f35a9401c2
Stan <stankv@mail.ru>
2025-04-05 12:05:39 +0300

    show popular posts v.2 added. Errors fixed.

--- siteblog/siteblog_app/templatetags/sidebar.py
+++ siteblog/siteblog_app/templatetags/sidebar.py
@@ -4,7 +4,7 @@ from siteblog_app.models import Post, Tag
 register = template.Library()
 
 @register.inclusion_tag('siteblog_app/popular_posts_tpl.html')
-def get_popular_posts(cnt=3):
+def get_popular_posts(cnt=3):    # кол-во выводимых постов
     posts = Post.objects.order_by('-count_views')[:cnt]
     return {"posts": posts}
 
